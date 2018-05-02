c2c23b5fe71f959831c310387c6fb0bffcb10ec1
Uzziel Perez <cuperez@fnal.gov>
2018-05-01 23:50:51 -0500

    using converted sherpa parameters

---
+++ LocalGENSIM/Pythia/TestADDGravToGG_MS-4000_NED-4_KK-1_13TeV.cc
@@ -0,0 +1,51 @@
+// Copyright (C) 2017 Torbjorn Sjostrand.
+// PYTHIA is licenced under the GNU GPL version 2, see COPYING for details.
+// Please respect the MCnet Guidelines, see GUIDELINES for details.
+
+// http://home.thep.lu.se/~torbjorn/pythia8/worksheet8153.pdf
+
+#include "Pythia8/Pythia.h"
+using namespace Pythia8;
+int main() {
+  // Generator. Process selection. Tevatron initialization. Histogram.
+  Pythia pythia;
+
+  // Beam Parameter Settings 
+  pythia.readString("Beams:idA = 2212"); //proton
+  pythia.readString("Beams:idB = 2212"); //proton
+  pythia.readString("Beams:eCM = 13000."); //CM energy of collision 
+  
+  // Settings for the Process Generation  
+  pythia.readString("ExtraDimensionsLED:ffbar2gammagamma = on");
+  pythia.readString("ExtraDimensionsLED:gg2gammagamma = on");
+  pythia.readString("PhaseSpace:pTHatMin = 70.");
+  
+  // LED parameters
+  pythia.readString("ExtraDimensionsLED:MD = 1128"); // Fundamental scale of gravity, Ms = 4.
+  pythia.readString("ExtraDimensionsLED:n = 2"); // number of extradimensions, default =2 
+  pythia.readString("ExtraDimensionsLED:LambdaT = 1128"); // Ultraviolet cutoff for the virtual graviton exchange
+  pythia.readString("ExtraDimensionsLED:CutoffMode = 2"); // 0 - all values of sHat contribute
+  							// 1 - truncate contributing sHat region
+							// 2 - Form factor using mu = renormScale2
+							// 3 - Form factor using mu = E_jet
+
+  // Initialize. 
+  pythia.init();
+
+  // Book histograms. 
+  Hist pTG("process pT scale", 100, 1000., 2000.);
+  
+  // Begin event loop. Generate event. Skip if error. List first one.
+  for (int iEvent = 0; iEvent < 1000; ++iEvent) {
+    if (!pythia.next()) continue;
+    // Loop over particles in event.Fill its pT.
+    int iG = 0;
+    for (int i = 0; i < pythia.event.size(); ++i)
+      if (pythia.event[i].id() == 22) iG = i;   // gamma ID = 22  
+    pTG.fill( pythia.event[iG].pT() );
+  // End of event loop. Statistics. Histogram. Done.
+  }
+  pythia.stat();
+  cout << pTG;
+  return 0;
+}
